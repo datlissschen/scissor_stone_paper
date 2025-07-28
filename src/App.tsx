9de42d60d378bc4a3448bd88ac88ee10cee15391
datlissschen <lisa.na2710@gmail.com>
2025-07-28 12:44:26 +0200

    build: create the mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import Greeting from './components/Greeting';
+import "./App.css"
+import { FaHandRock, FaHandPaper, FaHandScissors } from "react-icons/fa";
 
 const App: React.FC = () => {
     return (
-        <main>
-            <h1>Hello from App Component</h1>
-            <Greeting name={"Max"}/>
+        <main className = "center">
+            <h1> Schere Stein Papier</h1>
+            <div>
+                <div className = "container">
+                    <div className="player">
+                        <div className="score">
+                            Spieler 1:0
+                        </div>
+                        <div className="action">
+                            <FaHandRock size={60}/>
+                        </div>
+                    </div>
+                    <div className="player">
+                        <div className="score">
+                        Spieler 2:0
+                        </div>
+                        <div className = "action">
+                            <FaHandRock size={60} />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <button className = "round-btn"><FaHandScissors size={20}/></button>
+                    <button className = "round-btn"><FaHandRock size={20}/></button>
+                    <button className = "round-btn"><FaHandPaper size={20}/></button>
+                </div>
+                <div>
+                    Spieler 1 gewinnt!
+                </div>
+            </div>
         </main>
     );
 };
