b345682625d96c004ef3a355f4a8012b9460f4fa
datlissschen <lisa.na2710@gmail.com>
2025-08-04 18:42:40 +0200

    style: adding simple visual differences

--- src/components/ShowWinner.tsx
+++ src/components/ShowWinner.tsx
@@ -1,9 +1,9 @@
 
 export function ShowWinner({ winner }: { winner: number }) {
     const text: Record<number, string> = {
-        [-1]: "Du gewinnst!",
-        0: "Unentschieden",
-        1: "Du verlierst",
+        [-1]: "Du gewinnst! 🥳",
+        0: "Unentschieden ☯️",
+        1: "Du verlierst ☹️",
     };
 
     return <h2>{text[winner]}</h2>;
